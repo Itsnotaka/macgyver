08f88914fd218bb064d468b574826ffe932aa3e1
itsnotaka <itsnotaka@gmail.com>
2022-05-03 18:29:14 -0400

    Fixed old Tailwindcss packag problem

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// set a global scope for content to prevent No utility classes error
+const { join } = require('path');
+
+module.exports = {
+	content: [
+		`${join(__dirname, '/layers/renderer/src')}/**/*.{js,jsx,ts,tsx}`,
+		// `${join(__dirname, 'app')}/**/*.{js,jsx,ts,tsx}`,
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
